a7b517df360aec13b50d8b883a337e8c9f12f5cb
hpaanonlineuniversity <hpaanonlineuniversity@gmail.com>
2025-10-11 13:52:37 +0630

    create component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router"
+
+const header = () => {
+  return (
+    <div className='bg-slate-200'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+          <Link to='/'>
+            <h1 className='font-bold'>MERN STACK APP</h1>
+          </Link>
+          
+          <ul className='flex gap-4'>
+            <Link to='/'>
+              <li>Home</li>
+            </Link>
+            <Link to='/about'>
+              <li>About</li>
+            </Link>
+            <Link to='/sign-in'>
+              <li>SignIn</li>
+            </Link>
+          </ul>
+      </div>
+    </div>
+  )
+}
+
+export default header
